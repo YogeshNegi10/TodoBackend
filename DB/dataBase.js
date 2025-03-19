0268239751f75b7f894354d7f76218f7aa84f2f2
YogeshNegi10 <yogeshlm13@gmail.com>
2025-03-20 01:05:52 +0530

    changes

--- DB/dataBase.js
+++ DB/dataBase.js
@@ -1,18 +1,18 @@
-import dotenv from 'dotenv'
-import mongoose from 'mongoose'
+// import dotenv from 'dotenv'
+// import mongoose from 'mongoose'
 
-//Using dotenv to access Environmental varialbles
-dotenv.config();
-//Function To Connect with Datatbase
-const connectDb = () =>{
+// //Using dotenv to access Environmental varialbles
+// dotenv.config();
+// //Function To Connect with Datatbase
+// const connectDb = () =>{
 
-   mongoose.connect(process.env.MONGO_URI,{
-      dbName:'TodoApi',
-    }).then(()=>{
-        console.log(`DB Connected...`)
-    }).catch((err)=>{
-        console.log(err)
-    })
-}
+//    mongoose.connect(process.env.MONGO_URI,{
+//       dbName:'TodoApi',
+//     }).then(()=>{
+//         console.log(`DB Connected...`)
+//     }).catch((err)=>{
+//         console.log(err)
+//     })
+// }
 
-export default connectDb;
+// export default connectDb;
